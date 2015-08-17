66197bbe2fd00d6ef17711b247da4c73c266bdbb
Phillip Shannon & Ike Mattice <student@epicodus.com>
2015-08-17 12:18:52 -0700

    Update to app.php Task.php and TaskTest.php to add database functionality and create tests.

--- app/app.php
+++ app/app.php
@@ -1,13 +1,14 @@
 <?php
     require_once __DIR__."/../vendor/autoload.php";
-    require_once __DIR__."/../src/task.php";
-
-    session_start();
-    if (empty($_SESSION['list_of_tasks'])) {
-        $_SESSION['list_of_tasks'] = array();
-    }
+    require_once __DIR__."/../src/Task.php";
 
     $app = new Silex\Application();
+
+    $server = 'mysql:host=localhost;dbname=to_do';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
     $app->register(new Silex\Provider\TwigServiceProvider(), array(
         'twig.path' => __DIR__.'/../views'
     ));
